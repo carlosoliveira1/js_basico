7a5dec057a09a56f31d7cd424583099d482e604d
Carlos Oliveira <carlosoliveiramauricio@gmail.com>
2023-10-19 19:29:30 -0300

    ajuste do ccs

--- QAx/js/app.js
+++ QAx/js/app.js
@@ -1,1 +1,12 @@
-console.log('Olá, javascript')
+// console.log('Olá, javascript')
+
+// var nome = 'Mestre Yoda'
+// var idade = 100
+// var jedi = true
+
+// console.log(nome)
+// console.log(idade)
+// console.log(jedi)
+
+var username = "Fernando"
+document.getElementById('user-name').innerHTML = username
