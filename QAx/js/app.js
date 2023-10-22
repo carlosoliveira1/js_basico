ca94f10531a734e80bee1a55353cdcaefd631cfd
Carlos Oliveira <carlosoliveiramauricio@gmail.com>
2023-10-22 09:13:45 -0300

    Criando um filtro de busca

--- QAx/js/app.js
+++ QAx/js/app.js
@@ -1,53 +1,53 @@
 const LIST = [
 
-{
+    {
 
-    id: 1,
-    nome: 'Mestre Yoda',
-    avatar: 'images/yoda.png'
-},
-{
+        id: 1,
+        nome: 'Mestre Yoda',
+        avatar: 'images/yoda.png'
+    },
+    {
 
-    id: 2,
-    nome: 'Luke Skywalker',
-    avatar: 'images/luke.png'
-},
-{
+        id: 2,
+        nome: 'Luke Skywalker',
+        avatar: 'images/luke.png'
+    },
+    {
 
-    id: 3,
-    nome: 'Princesa Leia"',
-    avatar: 'images/leia.png'
-},
-{
+        id: 3,
+        nome: 'Princesa Leia"',
+        avatar: 'images/leia.png'
+    },
+    {
 
-    id: 4,
-    nome: 'Han Solo',
-    avatar: 'images/hansolo.png'
-},
-{
+        id: 4,
+        nome: 'Han Solo',
+        avatar: 'images/hansolo.png'
+    },
+    {
 
-    id: 5,
-    nome: 'Darth Vader',
-    avatar: 'images/vader.png'
-},
-{
+        id: 5,
+        nome: 'Darth Vader',
+        avatar: 'images/vader.png'
+    },
+    {
 
-    id: 6,
-    nome: 'Chewbacca',
-    avatar: 'images/chewbacca.png'
-},
-{
+        id: 6,
+        nome: 'Chewbacca',
+        avatar: 'images/chewbacca.png'
+    },
+    {
 
-    id: 7,
-    nome: 'R2D2',
-    avatar: 'images/r2d2.png'
-},
-{
+        id: 7,
+        nome: 'R2D2',
+        avatar: 'images/r2d2.png'
+    },
+    {
 
-    id: 8,
-    nome: 'Mestre Yoda',
-    avatar: 'images/yoda.png'
-},
+        id: 8,
+        nome: 'C3PO',
+        avatar: 'images/c3po.png'
+    },
 
 ]
 
@@ -56,12 +56,37 @@ const App = new Vue({
     data: {
         title: 'Star Wars Lego',
         userName: 'Carlos',
-        characters: LIST
+        characters: LIST,
+        searchName: ''
     },
     methods: {
 
         like(userName) {
             alert(`O personagem ${userName} recebeu um like`)
+
+        },
+
+        search() {
+
+            if(this.searchName === ''){
+                return alert('O campo de busca é obrigatório')
+            }
+
+
+
+
+            const list = this.characters = LIST
+
+            const result = list.filter(item => {
+                return item.nome === this.searchName
+            })
+
+            if (result.length <= 0) {
+                alert('Nenhum produto encontrado')
+            } else {
+                this.characters = result
+            }
+
 
         }
     }
